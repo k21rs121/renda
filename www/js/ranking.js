6467e3b1050a2c5de8b266beffdf0884ec11deb8
k21rs121 <k21rs121@st.kyusan-u.ac.jp>
2022-12-22 09:45:28 +0900

    ランキングの追加

--- www/js/ranking.js
+++ www/js/ranking.js
@@ -17,7 +17,21 @@ function toRanking() {
 // 【mBaaS】保存したデータの検索と取得
 function checkRanking() {
     // **********【問題２】ランキングを表示しよう！**********
-    
+    var highScore = ncmb.DataStore("GameScore");
+// scoreの降順でデータ5件を取得するように設定する
+highScore.order("score", true)
+         .limit(5)
+         .fetchAll()
+         .then(function(results){
+             // 検索に成功した場合の処理
+             console.log("検索に成功しました。");
+             // テーブルにデータをセット
+             setData(results);
+         })
+         .catch(function(error){
+             // 検索に失敗した場合の処理
+             console.log("検索に失敗しました。エラー:" +error);
+         });
     
     
     
